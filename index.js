c1d0a86c7c0a28294c8ebe5360c769e0ac8a19fd
B Naga Sai Yashwanth <b.10741830@ltimindtree.com>
2024-06-13 17:20:12 +0530

    first change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello my name is Yashwanth\n'
+  const msg = 'Hello I am Yashwanth Devops trainee at LTIMindtree\n'
   res.end(msg);
 });
 
