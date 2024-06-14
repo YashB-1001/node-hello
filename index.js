ec8eef205903c6f6f0882bee551a436c3cd17f29
B Naga Sai Yashwanth <b.10741830@ltimindtree.com>
2024-06-14 11:17:47 +0530

    first change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello I am B.Naga Sai Yashwanth\n'
+  const msg = 'Hi I am B.Naga Sai Yashwanth\n'
   res.end(msg);
 });
 
